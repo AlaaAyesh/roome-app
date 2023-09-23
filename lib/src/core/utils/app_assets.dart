fc7a62a12dd80f6fa63fcec70b28afaa82f3444e
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-09-23 04:50:40 +0300

    Add AppAssets & Change app name , icon

---
+++ lib/src/core/utils/app_assets.dart
@@ -0,0 +1,71 @@
+class AppAssets {
+  AppAssets._();
+
+  /// Assets for AppIcon
+  /// assets/appIcon.svg
+  static const String appIcon = "assets/appIcon.png";
+
+  /// Assets for iconAppleIcon
+  /// assets/icon/appleIcon.svg
+  static const String iconAppleIcon = "assets/icon/appleIcon.svg";
+
+  /// Assets for iconGoogleIcon
+  /// assets/icon/googleIcon.svg
+  static const String iconGoogleIcon = "assets/icon/googleIcon.svg";
+
+  /// Assets for iconHotelBookingCanceledIcon
+  /// assets/icon/hotelBookingCanceledIcon.svg
+  static const String iconHotelBookingCanceledIcon =
+      "assets/icon/hotelBookingCanceledIcon.svg";
+
+  /// Assets for iconPasswordSuccessfullyChangedIcon
+  /// assets/icon/passwordSuccessfullyChangedIcon.svg
+  static const String iconPasswordSuccessfullyChangedIcon =
+      "assets/icon/passwordSuccessfullyChangedIcon.svg";
+
+  /// Assets for iconPaymentSuccessfulIcon
+  /// assets/icon/paymentSuccessfulIcon.svg
+  static const String iconPaymentSuccessfulIcon =
+      "assets/icon/paymentSuccessfulIcon.svg";
+
+  /// Assets for imageBlueCircles
+  /// assets/image/blueCircles.svg
+  static const String imageBlueCircles = "assets/image/blueCircles.svg";
+
+  /// Assets for imageCreditCard
+  /// assets/image/creditCard.svg
+  static const String imageCreditCard = "assets/image/creditCard.svg";
+
+  /// Assets for imageGreenCircles
+  /// assets/image/greenCircles.svg
+  static const String imageGreenCircles = "assets/image/greenCircles.svg";
+
+  /// Assets for imageLightBlueCircles
+  /// assets/image/lightBlueCircles.svg
+  static const String imageLightBlueCircles =
+      "assets/image/lightBlueCircles.svg";
+
+  /// Assets for imageOnBoarding1
+  /// assets/image/onBoarding1.svg
+  static const String imageOnBoarding1 = "assets/image/onBoarding1.svg";
+
+  /// Assets for imageOnBoarding2
+  /// assets/image/onBoarding2.svg
+  static const String imageOnBoarding2 = "assets/image/onBoarding2.svg";
+
+  /// Assets for imageOnBoarding3
+  /// assets/image/onBoarding3.svg
+  static const String imageOnBoarding3 = "assets/image/onBoarding3.svg";
+
+  /// Assets for imageQrCode
+  /// assets/image/qrCode.svg
+  static const String imageQrCode = "assets/image/qrCode.svg";
+
+  /// Assets for imageRedCircles
+  /// assets/image/redCircles.svg
+  static const String imageRedCircles = "assets/image/redCircles.svg";
+
+  /// Assets for imageSplashCover
+  /// assets/image/splashCover.svg
+  static const String imageSplashCover = "assets/image/splashCover.svg";
+}
