4dd7b51df60a4d42d3629a5a75158052a3671f80
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2023-09-25 01:34:45 +0300

    Build some reusable widgets

---
+++ lib/src/core/widgets/visibility_icon_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:roome/src/core/utils/app_colors.dart';
+
+class VisibilityIconButton extends StatelessWidget {
+  const VisibilityIconButton({
+    Key? key,
+    required this.icon,
+    this.onPressed,
+  }) : super(key: key);
+
+  final void Function()? onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: IconButton(
+        splashColor: AppColors.textFieldIconColor.withOpacity(0.2),
+        onPressed: onPressed,
+        icon: Icon(icon),
+      ),
+    );
+  }
+}
